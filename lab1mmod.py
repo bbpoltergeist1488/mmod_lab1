28d66056a020373c45a0d09c656a6dc64707d3aa
bbpoltergeist1488 <48241197+bbpoltergeist1488@users.noreply.github.com>
2021-11-09 23:23:55 +0300

    Add files via upload

--- lab1mmod.py
+++ lab1mmod.py
@@ -48,11 +48,7 @@ def disp_dot(type, size, array, exp, vib):
     S_2 = 0
     for i in range(size):
         S_2 += (array[i] - exp) ** 2
-    S_2 /= len(array)
-    if type == 'X':
-        print('Точечная оценка D[X]:', S_2)
-    else:
-        print('Точечная оценка D[Y]:', S_2)
+    S_2 /= vib
     return S_2
 
 
@@ -61,21 +57,25 @@ def math_exp_int(exp, disp, test, vib):
     return [exp - exp_int, exp + exp_int]
 
 
-def disp_int(vib, disp):
-    chi1 = 304.9
-    chi2 = 200.9
+def disp_int(vib, disp, alpha):
+    chi1 = scipy.stats.chi2.ppf(alpha + (1 - alpha) / 2, df=vib - 1)
+    chi2 = scipy.stats.chi2.ppf((1 - alpha) / 2, df=vib - 1)
     d_int = (vib - 1) * disp
     return [d_int / chi1, d_int / chi2]
 
 
-def correlation(arrayX, arrayY, expX, expY, sizeX, sizeY, dispX, dispY):
+def correlation(arrayX, arrayY, expX_emp, expY_emp, sizeX, sizeY, dispX, dispY):
     Mx = 0
     My = 0
+    Dx = 0
+    Dy = 0
     for i in range(sizeX):
-        Mx += (arrayX[i] - expX)
+        Mx += (arrayX[i] - expX_emp)
+        Dx += (arrayX[i] - expX_emp) ** 2
     for i in range(sizeY):
-        My += (arrayY[i] - expY)
-    result = (Mx * My) / ((math.sqrt(dispX) * math.sqrt(dispY)))
+        My += (arrayY[i] - expY_emp)
+        Dy += (arrayY[i] - expY_emp) ** 2
+    result = (Mx * My) / ((math.sqrt(Dx) * math.sqrt(Dy)))
     return result / (math.sqrt(sizeX) * math.sqrt(sizeY))
 
 
@@ -83,28 +83,30 @@ def criteria(matrix_t, matrix_emp, size_x, size_y, vib):
     crit_x = 0
     crit_y = 0
     for i in range(size_x):
-        crit_x = (sum(matrix_t[i]) - sum(matrix_emp[i] / vib)) ** 2 / sum(matrix_emp[i] / vib)
+        crit_x = (sum(matrix_t[i]) - sum(matrix_emp[i])) ** 2 / sum(matrix_emp[i])
     temp_mat = copy.copy(matrix_emp)
     temp_mat = temp_mat.transpose()
-
+    temp_th = copy.copy(matrix_t)
+    temp_th = temp_th.transpose()
     for i in range(size_y):
-        crit_y = (sum(matrix_t[i]) - sum(temp_mat[i] / vib)) ** 2 / sum(temp_mat[i] / vib)
+        crit_y = (sum(temp_th[i]) - sum(temp_mat[i])) ** 2 / sum(temp_mat[i])
     return crit_x, crit_y
 
 
-# n = int(input("n: "))
-# m = int(input("m: "))
-# rnd = int(input("rnd: "))
-n = 6
-m = 6
-rnd = 250
-xi = 11.34
+n = int(input("n: "))
+m = int(input("m: "))
+rnd = int(input("rnd: "))
+# n = 6
+# m = 6
+# rnd = 250
+alpha = 0.01
+
 prob = []
 emp_matrix = np.zeros((n, m))
 array = np.array(np.random.dirichlet(np.ones(n * m)) * 1)
-matrix = np.reshape(array, (n, m))
+theory_matrix = np.reshape(array, (n, m))
 print("Теоретическая матрица распределения:")
-print(matrix)
+print(theory_matrix)
 
 for i in range(rnd):
     a = random()
@@ -112,7 +114,7 @@ for i in range(rnd):
     skip = False
     for j in range(n):
         for k in range(m):
-            summ += matrix[j][k]
+            summ += theory_matrix[j][k]
 
             if summ >= a:
                 emp_matrix[j][k] += 1
@@ -123,52 +125,124 @@ for i in range(rnd):
         if skip:
             break
 
+for i in range(n):
+    for j in range(m):
+        emp_matrix[i][j] /= rnd
+
 emp_copy = np.reshape(copy.copy(emp_matrix), (1, n * m))[0]
 prob = copy.copy(array)
 prob_str = []
 for i in range(len(array)):
     prob_str.append(str(prob[i]))
 print()
-print("Эмпирическая матрица распределния:")
+
+print("Эмпирическая матрица распределения:")
 print(emp_matrix)
+X = []
+Y = []
+vib_X = []
+vib_Y = []
+for i in range(n):
+    X.append(sum(emp_matrix[i]))
+    vib_X.append(X[i] * rnd)
+
+temp_emp = copy.copy(emp_matrix)
+temp_emp = temp_emp.transpose()
+
+for i in range(m):
+    Y.append(sum(temp_emp[i]) * rnd)
+    vib_Y.append(Y[i] * rnd)
+X_str = []
+Y_str = []
+for i in range(n):
+    X_str.append(str(X[i]))
+for i in range(m):
+    Y_str.append(str(Y[i]))
+
 fig, ax = plt.subplots(figsize=(15, 7))
-result = ax.bar(prob_str, emp_copy)
+fig1, ax1 = plt.subplots(figsize=(15, 7))
+result = ax.bar(X_str, vib_X)
+result2 = ax1.bar(Y_str, vib_Y)
 plt.show()
 
 arrX = arrayXY(n)
 arrY = arrayXY(m)
 print('\n')
 
-expX = math_exp_X(arrX, emp_matrix, n, m, rnd)
-expY = math_exp_Y(arrY, emp_matrix, n, m, rnd)
+expX_theory = math_exp_X(arrX, theory_matrix, n, m, rnd)
+expY_theory = math_exp_Y(arrY, theory_matrix, n, m, rnd)
 
-print("Точечная оценка M[X]:", expX)
-print("Точечная оценка M[Y]:", expY)
+expX_emp = math_exp_X(arrX, emp_matrix, n, m, rnd)
+expY_emp = math_exp_Y(arrY, emp_matrix, n, m, rnd)
 
-D_x = disp_dot('X', n, arrX, expX, rnd)
-D_y = disp_dot('Y', m, arrY, expY, rnd)
+print("Теоретическая точечная оценка M[X]:", expX_theory)
+print("Теоретическая точечная оценка M[Y]:", expY_theory)
+print()
+print("Эмпирическая точечная оценка M[X]:", expX_emp)
+print("Эмпирическая точечная оценка M[Y]:", expY_emp)
 
+D_x_theory = disp_dot('X', n, arrX, expX_theory, rnd)
+D_y_theory = disp_dot('Y', m, arrY, expY_theory, rnd)
+print()
+print("Теоретическая точечная оценка D[X]:", D_x_theory)
+print("Теоретическая точечная оценка D[Y]:", D_y_theory)
+
+D_x_emp = disp_dot('X', n, arrX, expX_emp, rnd)
+D_y_emp = disp_dot('Y', m, arrY, expY_emp, rnd)
+print()
+print("Эмпирическая точечная оценка D[X]:", D_x_emp)
+print("Эмпирическая точечная оценка D[Y]:", D_y_emp)
+print()
 test_st = student_test(0.01, rnd - 1)
 
-math_expX_int = math_exp_int(expX, D_x, test_st, rnd)
-math_expY_int = math_exp_int(expY, D_y, test_st, rnd)
+math_expX_int_emp = math_exp_int(expX_emp, D_x_emp, test_st, rnd)
+math_expY_int_emp = math_exp_int(expY_emp, D_y_emp, test_st, rnd)
 
-print("Интервальная оценка M[X]:", math_expX_int)
-print("Интервальная оценка M[Y]:", math_expY_int)
+math_expX_int_th = math_exp_int(expX_theory, D_x_theory, test_st, rnd)
+math_expY_int_th = math_exp_int(expY_theory, D_y_theory, test_st, rnd)
 
-disp_intX = disp_int(rnd, D_x)
-disp_intY = disp_int(rnd, D_y)
+print("Теоретическая интервальная оценка M[X]:", math_expX_int_th)
+print("Теоретическая интервальная оценка M[Y]:", math_expY_int_th)
+print()
+print("Эмпирическая интервальная оценка M[X]:", math_expX_int_emp)
+print("Эмпирическая интервальная оценка M[Y]:", math_expY_int_emp)
 
-print("Интервальная оценка D[X]:", disp_intX)
-print("Интервальная оценка D[Y]:", disp_intY)
+disp_intX_th = disp_int(rnd, D_x_theory, 0.01)
+disp_intY_th = disp_int(rnd, D_y_theory, 0.01)
 
-print("Коэффицент корреляции:", correlation(arrX, arrY, expX, expY, n, m, D_x, D_y))
+disp_intX_emp = disp_int(rnd, D_x_emp, 0.01)
+disp_intY_emp = disp_int(rnd, D_y_emp, 0.01)
+print()
+print("Теоретическая интервальная оценка D[X]:", disp_intX_th)
+print("Теоретическая интервальная оценка D[Y]:", disp_intY_th)
+print()
+print("Эмпирическая интервальная оценка D[X]:", disp_intX_emp)
+print("Эмпирическая интервальная оценка D[Y]:", disp_intY_emp)
 
-critX, critY = criteria(matrix, emp_matrix, n, m, rnd)
+print("Теоретический коэффицент корреляции:",
+      correlation(arrX, arrY, expX_theory, expY_theory, n, m, D_x_theory, D_y_theory))
+
+print("Эмпирический коэффицент корреляции:", correlation(arrX, arrY, expX_emp, expY_emp, n, m, D_x_emp, D_y_emp))
+
+critX, critY = criteria(theory_matrix, emp_matrix, n, m, rnd)
+
+xiX = scipy.stats.chi2.ppf(alpha, df=n - 3)
+xiY = scipy.stats.chi2.ppf(alpha, df=m - 3)
+checkX = False
+checkY = False
 print("Проверка гипотезы по критерию согласия Пирсона:")
 print()
-print("Chi^2(alpha = 0.01, k = 3):", xi)
-print("Критерий Пирсона для X:",critX)
-print("Критерий Пирсона для Y:",critY)
+print("X - Chi^2:", xiX)
+print("Критерий Пирсона для X:", critX)
 print()
-print("Chi^2(alpha, k) > chi^2(X) & Chi^2(alpha, k) > chi^2(Y), значит, нет оснований отклонять гипотезу о соответствии полученных оценок характеристик СВ требуемым")
+print("Y - Chi^2:", xiY)
+print("Критерий Пирсона для Y:", critY)
+if xiX >= critX:
+    checkX = True
+if xiY >= critY:
+    checkY = True
+print()
+print("Проверка гипотезы о соответствии полученных оценок характеристик СВ требуемым:")
+print()
+print("X -", checkX)
+print("Y -", checkY)
